9d6e27f34a2c4a00e893696675e9f03270c94242
Isaiah <isaiahrodriguez99@yahoo.com>
2021-09-30 13:40:05 -0500

    Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+const Nav = () => {
+    return ( 
+        <nav>
+            <div>
+                <h1>Life Strategies</h1>
+            </div>
+
+            <ul className="links">
+                <li><a>Counseling</a></li>
+                <li><a>Coaching</a></li>
+                <li><a>Women's Health</a></li>
+                <li><a>Faith Based</a></li>
+                <li><a>About</a></li>
+            </ul>     
+        </nav>
+     );
+}
+ 
+export default Nav;

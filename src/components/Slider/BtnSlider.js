ecb5616a6f73da84c9d7ebaab327a5c5009b4288
Isaiah <isaiahrodriguez99@yahoo.com>
2021-09-30 15:43:33 -0500

    button component done

---
+++ src/components/Slider/BtnSlider.js
@@ -0,0 +1,13 @@
+import './Slider.css'
+// import leftArrow from '..icons/left-arrow.svg'
+import rightArrow from '../icons/right-arrow.svg'
+
+const BtnSlider = ({direction, moveSlide}) => {
+    return ( 
+        <button onCLick={moveSlide} className={direction === "next" ? 'btn-slide next' : "btn-slide prev"}>
+            <img src={direction === "next" ? rightArrow : leftArrow} />
+        </button>
+     );
+}
+ 
+export default BtnSlider;

8991e056b195c8ca647a4b4272786ae02874993f
Isaiah <isaiaharod99@gmail.com>
2021-10-01 20:37:11 -0500

    Slider Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './components/Nav'
+import Slider from './components/Slider/Slider'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Slider />
     </div>
   );
 }

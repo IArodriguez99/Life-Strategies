b352239805367797a45ffc50e2506aeae24a82a8
Isaiah <isaiaharod99@gmail.com>
2021-10-12 23:34:00 -0500

    Landing page update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,13 @@
 import './Home.css'
-import headshot from './Maria.png'
+import headshot from './Holding.png'
 
 const Home = () => {
     return ( 
         <div className="container-home">
             <div className="home-row">
                 <div className="home-col">
-                    <h1>Lorem ipsum dolor sit consect amet.</h1>
-                    <p>Ut lectus arcu bibendum at varius vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet.</p>
+                    <h1>Determined to strategize a better life.</h1>
+                    <p className="home-p">Working together to transform the emotional, psychological and spiritual well-being of individuals, families, organizations, and communities.</p>
                     <button className="btn1">Make an Appointment</button>
                 </div>
                 <div>

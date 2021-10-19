4b6e7caa2ea92ef1b6fb958dc446afaaf1ceb32e
Isaiah <isaiaharod99@gmail.com>
2021-10-18 21:26:15 -0500

    Adding to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav/Nav'
 import Home from './components/Home/Home'
 import Form from './components/Form/Form';
 import About from './components/About/About'
+import Service from './components/Service/Service'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Home />
       <About />
+      <Service />
       {/* <Form /> */}
     </div>
   );

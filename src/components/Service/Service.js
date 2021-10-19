973657e7147e7ab9040936c3f2d7019280d3f17d
Isaiah <isaiaharod99@gmail.com>
2021-10-18 21:21:03 -0500

    Service Component

---
+++ src/components/Service/Service.js
@@ -0,0 +1,28 @@
+import './Service.css'
+
+const Service = () => {
+    return ( 
+        <div className="service-container">
+            <div className="service-grid">
+                <div className="service-item">
+                    <h2>Counseling</h2>
+                    <p>Working together to transform the emotional, psychological and spiritual well-being of individuals, families and communities.</p>
+                </div>
+                <div className="service-item">
+                    <h2>Coaching</h2>
+                    <p>Working together to transform the emotional, psychological and spiritual well-being of individuals, families and communities.</p>
+                </div>
+                <div className="service-item">
+                    <h2>Women's Health</h2>
+                    <p>Working together to transform the emotional, psychological and spiritual well-being of individuals, families and communities.</p>
+                </div>
+                <div className="service-item">
+                    <h2>Faith Based</h2>
+                    <p>Working together to transform the emotional, psychological and spiritual well-being of individuals, families and communities.</p>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Service;

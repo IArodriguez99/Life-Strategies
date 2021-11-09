13510cead3a36fee6e83ac6af9c49a2fa1b91284
Isaiah <isaiaharod99@gmail.com>
2021-11-08 21:42:18 -0600

    Counseling page

---
+++ src/components/Counseling/Counseling.js
@@ -0,0 +1,17 @@
+import './Counseling.css'
+
+const Counseling = () => {
+    return ( 
+        <div className="counseling-container">
+            <div className="counseling-row">
+                <div className="counseling-col">
+                    <h1>Our creative and interactive approach is sure to engage you on every level.</h1>
+                    <p>You can rely on us for complete mental health counseling. We offer support for those struggling with obsessive compulsive disorder, PTSD, trauma, sexual abuse, and other mental health disorders. Experience the relief you have been searching for as we explore your past and make plans for your bright and promising future.</p>
+                    <button className="btn1">Make an Appointment</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Counseling;

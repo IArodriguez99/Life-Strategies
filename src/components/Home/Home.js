5c4bb53877634bc9c2c5c86c0f7d10dc4bbb8687
Isaiah <isaiaharod99@gmail.com>
2021-11-11 19:10:21 -0600

    Text change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="container-home">
             <div className="home-row">
                 <div className="home-col">
-                    <h1>Determined to strategize a better life.</h1>
+                    <h1>Building bridges for successful living</h1>
                     <p className="home-p">Working together to transform the emotional, psychological and spiritual well-being of individuals, families, organizations, and communities.</p>
                     <button className="btn1">Make an Appointment</button>
                 </div>

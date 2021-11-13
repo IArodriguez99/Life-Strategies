84568a8776cc19a7a64d77207b99235ba6c086f9
Isaiah <isaiaharod99@gmail.com>
2021-11-12 21:53:20 -0600

    Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import './Footer.css'
+
+const Footer = () => {
+    return ( 
+        <div className="footer-container">
+            <div className="footer-row">
+                <div className="footer-col">
+                    <h1>Life Strategies</h1>
+                    <p>“Building Bridges For Successful Living”</p>
+                </div>
+                <div className="footer-col">
+                    <p>Forms</p>
+                    <p>Policy-(HIPAA)</p>
+                    <p>Mandatory Reporting Requirements</p>
+                    <p>Financial and Missed Appointment Policy</p>
+                    <p>Informed Consent Form</p>
+                </div>
+                <div className="footer-col">
+                    <p>Contact</p>
+                    <p>Email: mfernandez916@gmail.com</p>
+                    <p>Phone: 407-738-0955</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Footer;

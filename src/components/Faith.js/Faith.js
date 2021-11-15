570e3e28a574920196455128600c831baaf56e04
Isaiah <isaiaharod99@gmail.com>
2021-11-14 19:56:40 -0600

    Faith Hero

--- src/components/Faith.js/Faith.js
+++ src/components/Faith.js/Faith.js
@@ -2,10 +2,17 @@ import './Faith.css'
 
 const Faith = () => {
     return ( 
-            <div className="faith-container">
-                
+            <div className="faith-background">
+                <div className="faith-container">
+                    <div className="faith-row">
+                        <div className="faith-col">
+                            <h1>Faith Based Approach</h1>
+                            <p>As a member of the American Association of Christian Counselors I personally have a Christian Worldview in my approach to life.  As a therapist I do not impose my beliefs on my clients.  However, for those clients who want to integrate prayer and spirituality to some aspects of therapy I am flexible and will work with you.  Foundational theological issues should be managed by your Pastor and/or Clergy in the church where you attend.  I am respectful of denominational and theological beliefs.</p>
+                        </div>  
+                    </div>
+                </div>
             </div>
-    );
+    );      
 }
  
 export default Faith;

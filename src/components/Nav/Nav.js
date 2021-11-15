207eeeddc5def5a0df4538908b3f12d34790a427
Isaiah <isaiaharod99@gmail.com>
2021-11-14 19:57:02 -0600

    nav background color

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,21 +2,23 @@ import './Nav.css'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
-    return ( 
-        <nav>
-            <div>
-                <Link to= '/'><h2>Life Strategies</h2></Link>
-            </div>
+    return (
+        <div className="nav-container">
+            <nav>
+                <div>
+                    <Link to= '/'><h2>Life Strategies</h2></Link>
+                </div>
 
-            <ul className="links">
-                <Link to='/counseling'><li>Counseling</li></Link>
-                <li><a>Coaching</a></li>
-                <li><a>Women's Health</a></li>
-                <Link to='/faith'><li>Faith Based</li></Link>
-                <li><a>About</a></li>
-                <button className="nav-btn">Contact</button>
-            </ul>     
-        </nav>
+                <ul className="links">
+                    <Link to='/counseling'><li>Counseling</li></Link>
+                    <li><a>Coaching</a></li>
+                    <li><a>Women's Health</a></li>
+                    <Link to='/faith'><li>Faith Based</li></Link>
+                    <li><a>About</a></li>
+                    <button className="nav-btn">Contact</button>
+                </ul>     
+            </nav>
+        </div>
      );
 }
  
